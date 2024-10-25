cc2616ef0d3580027fa231988f3cf01c4d108a84
1Gould <danieligould@gmail.com>
2024-10-24 21:58:58 -0400

    24/10/24

--- get_remote_process.zig
+++ get_remote_process.zig
@@ -7,13 +7,41 @@ const c = @cImport({
     @cInclude("tlhelp32.h");
 });
 
-const ProcessIdentifier = struct {
-    processId: win.DWORD,
-    handle: win.HANDLE,
-};
+extern "kernel32" fn OpenProcess(
+    dwDesiredAccess: win.DWORD,
+    bInheritHandle: win.BOOL,
+    dwProcessId: win.DWORD,
+) callconv(win.WINAPI) win.HANDLE;
+
+pub extern "kernel32" fn WaitForSingleObject(
+    hHandle: win.HANDLE,
+    dwMilliseconds: win.DWORD,
+) callconv(win.WINAPI) win.DWORD;
 
 
-pub fn GetRemoteProcessHandle(szProcessName: []const u8) !ProcessIdentifier {
+pub fn main() !void {
+    const processName = "Notepad.exe";
+    var hProcess: win.HANDLE = undefined;
+    const pProcess: *win.HANDLE = &hProcess;
+    const dwProcessId: *u32 = undefined;
+
+    _ = c.SetConsoleOutputCP(c.CP_UTF8);
+
+    if (!GetRemoteProcessHandle(processName, dwProcessId, pProcess)) {
+        std.debug.print("[-] GetRemoteProcessHandle() failed.\n", .{});
+    }
+    std.debug.print("[+] GetRemoteProcessHandle() succeeded.\n", .{});
+
+    _ = WaitForSingleObject(hProcess, win.INFINITE);
+}
+
+pub fn exit() noreturn {
+    std.debug.print("Exiting...\n", .{});
+    @import("std").process.exit(0);
+}
+
+
+pub fn GetRemoteProcessHandle(szProcessName: []const u8, dwProcessId: *u32, pProcess: *win.HANDLE) bool {
     // Define our length constant
     const szProcessNameLength = szProcessName.len;
 
@@ -44,75 +72,50 @@ pub fn GetRemoteProcessHandle(szProcessName: []const u8) !ProcessIdentifier {
     const hSnapShot = c.CreateToolhelp32Snapshot(c.TH32CS_SNAPPROCESS, 0);
     if (hSnapShot == c.INVALID_HANDLE_VALUE or hSnapShot == null) {
         std.debug.print("[+] Unable to create snapshot, CreateToolhelp32Snapshot() failed.\n", .{});
-        return error.SnapshotCreationFailed;
+        return false;
     }
 
-    var hProcess: ?win.HANDLE = null;
-    var dwProcessId: win.DWORD = 0;
     var bRet = c.Process32First(hSnapShot, &Proc);
 
     std.debug.print("[+] Looking for process: {s}\n", .{pName[0..szProcessNameLength]});
 
-        while (bRet != 0) { // While there are processes, keep looping
+    while (bRet != 0) { // While there are processes, keep looping
 
-            // convert Proc.szExeFile to lower case
-            var j: usize = 0;
-            while (j < c.MAX_PATH and Proc.szExeFile[j] != 0) {
-                Proc.szExeFile[j] = std.ascii.toLower(Proc.szExeFile[j]);
-                j += 1;
-            }
-            // cast szExeFile to a sentinel-terminated pointer and create a slice
-            const temp: [*c]u8 = @ptrCast(&Proc.szExeFile);
-            const exeFileName = std.mem.span(temp);
-            std.debug.print("[+] Found process: {s}\n", .{exeFileName});
-            std.debug.print("[+] Found process ID: {d}\n", .{Proc.th32ProcessID});
-
-            // compare the process name to the name we are looking for
-            std.debug.print("[+] Comparing process name: {s} to {s}\n", .{exeFileName, pName[0..szProcessNameLength]});
-            // std.debug.print("Length of Proc.szExeFile: {s}\n", .{&Proc.szExeFile});
-            // std.debug.print("Length of pName slice: {d}\n", .{szProcessNameLength});
-            // std.debug.print("Raw bytes of Proc.szExeFile: {x}\n", .{&Proc.szExeFile});
-            // std.debug.print("Raw bytes of pName slice: {x}\n", .{pName[0..szProcessNameLength]});
-
-            if (std.mem.eql(u8, exeFileName, pName[0..szProcessNameLength])) {
-                dwProcessId = Proc.th32ProcessID;
-                hProcess = c.OpenProcess(c.PROCESS_ALL_ACCESS, win.FALSE, dwProcessId);
-                if (hProcess == null) {
-                    _ = c.CloseHandle(hSnapShot);
-                    std.debug.print("[+] Unable to open process: {s}\n", .{exeFileName});
-                    return error.ProcessOpenFailed;
-                }
-                break;
-            }
-            bRet = c.Process32Next(hSnapShot, &Proc);
+        // convert Proc.szExeFile to lower case
+        var j: usize = 0;
+        while (j < c.MAX_PATH and Proc.szExeFile[j] != 0) {
+            Proc.szExeFile[j] = std.ascii.toLower(Proc.szExeFile[j]);
+            j += 1;
         }
-    
+        // cast szExeFile to a sentinel-terminated pointer and create a slice
+        const temp: [*c]u8 = @ptrCast(&Proc.szExeFile);
+        const exeFileName = std.mem.span(temp);
+        std.debug.print("[+] Found process: {s}\n", .{exeFileName});
+        std.debug.print("[+] Found process ID: {d}\n", .{Proc.th32ProcessID});
+
+        // compare the process name to the name we are looking for
+        std.debug.print("[+] Comparing process name: {s} to {s}\n", .{ exeFileName, pName[0..szProcessNameLength] });
+
+        if (std.mem.eql(u8, exeFileName, pName[0..szProcessNameLength])) {
+            std.debug.print("[+] Assigning ProcessID.\n", .{});
+			// TODO: figure out how to cast this correctly, segfault
+            // dwProcessId.* = Proc.th32ProcessID;
+            _ = dwProcessId;
+
+            // Open the process
+            std.debug.print("[+] Opening Process.\n", .{});
+            const processHandle = OpenProcess(0x1fffff, 0, Proc.th32ProcessID);
+            std.debug.print("[+] Assigning Process Handle.\n", .{});
+            pProcess.* = processHandle;
+            break;
+        }
+        bRet = c.Process32Next(hSnapShot, &Proc);
+    }
 
     if (c.CloseHandle(hSnapShot) == 0) {
         std.debug.print("[+] Unable to close snapshot, CloseHandle() failed.\n", .{});
-        return error.SnapshotCloseFailed;
+        return false;
     }
 
-    if (hProcess) |validHandle| {
-        std.debug.print("[+] Successfully retrieved handle.\n", .{});
-
-        return ProcessIdentifier{
-            .handle = validHandle,
-            .processId = dwProcessId,
-        };
-    } else {
-        // Handle the case where hProcess is null
-        std.debug.print("[+] Unable to find process: {s}\n", .{pName[0..szProcessNameLength]});
-        return error.ProcessNotFound;
-    }
+    return true;
 }
-
-pub fn main() !void {
-    const processName = "Notepad.exe";
-
-    _ = c.SetConsoleOutputCP(c.CP_UTF8);
-
-    const result = try GetRemoteProcessHandle(processName);
-    std.debug.print("----------------------------\n[+] Process handle: {x}\n[+] Process ID: {d}\n----------------------------\n", .{&(result.handle), result.processId});
-}
-
